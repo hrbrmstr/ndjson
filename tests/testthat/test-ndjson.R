2002f1ec5595cefbf24a4b9ebd8ec7293f49bbcf
Bob Rudis <bob@rudis.net>
2016-08-27 00:07:17 -0400

    tests

--- tests/testthat/test-ndjson.R
+++ tests/testthat/test-ndjson.R
@@ -1,6 +1,12 @@
 context("basic functionality")
 test_that("we can do something", {
 
-  #expect_that(some_function(), is_a("data.frame"))
+  f <- system.file("extdata", "test.json", package="ndjson")
+  expect_that(validate(f), equals(TRUE))
+  expect_that(nrow(stream_in(f)), equals(100))
+
+  gzf <- system.file("extdata", "testgz.json.gz", package="ndjson")
+  expect_that(validate(gzf), equals(TRUE))
+  expect_that(nrow(stream_in(gzf)), equals(100))
 
 })

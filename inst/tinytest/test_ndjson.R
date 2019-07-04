8f0f3be3286d2c639aa63e287b11bc6d5e03ae03
hrbrmstr <bob@rud.is>
2019-07-04 11:49:57 -0400

    switch to tinytest; add code coverage

---
+++ inst/tinytest/test_ndjson.R
@@ -0,0 +1,13 @@
+library(ndjson)
+
+f <- system.file("extdata", "test.json", package="ndjson")
+expect_true(validate(f))
+expect_equal(nrow(stream_in(f)), 100)
+expect_true(inherits(stream_in(f), "tbl_dt"))
+expect_true(inherits(stream_in(f, "tbl"), "tbl_df"))
+
+gzf <- system.file("extdata", "testgz.json.gz", package="ndjson")
+expect_true(validate(gzf))
+expect_equal(nrow(stream_in(gzf)), 100)
+expect_true(inherits(stream_in(gzf), "tbl_dt"))
+expect_true(inherits(stream_in(gzf, "tbl"), "tbl_df"))
